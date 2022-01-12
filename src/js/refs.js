f4a2dc92adbbdc3a110576ad24d4152ff94aa5fd
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-01-12 17:09:47 +0200

    first commit

---
+++ src/js/refs.js
@@ -0,0 +1,17 @@
+export default {
+    // ---- header ---- 
+    headerLogo: document.querySelector('.header-logo-js'),
+    headerLogoIcon: document.querySelector('.header-logo-icon-js'),
+    headerHome: document.querySelector('.header-home-js'),
+    headerMyLibrary: document.querySelector('.header-my-library-js'),
+    headerWatchedBtn: document.querySelector('.header-watched-btn-js'),
+    headerQueuedBtn: document.querySelector('.header-queue-btn-js'),
+    headerFormInput: document.querySelector('.header-form-input-js'),
+    headerFormSubmitBtn: document.querySelector('.header-form-submit-btn-js'),
+    headerError: document.querySelector('.header-error-js'),
+    // ---- header для смены вида ----
+    headerSection: document.querySelector('.header-section-js'),
+    headerForm: document.querySelector('.header-form-js'),
+    headerLnWrapper: document.querySelector('.header-ln-wrapper-js'),
+    headerNavMyLibrary: document.querySelector('.header-nav-my-library-js')
+}
